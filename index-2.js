c2ef680d3992a025a775c17899be1d11482637f4
Yashesh1195 <155947815+Yashesh1195@users.noreply.github.com>
2024-05-03 16:56:17 +0530

    Add files via upload

---
+++ index-2.js
@@ -0,0 +1,12 @@
+// document.querySelector("#one").style.color="blue";
+// console.log(document.querySelector("#one").style.color="blue")
+// document.querySelector("#two").style.color="red";
+// console.log(document.querySelector("#two").style.color="red")
+// document.querySelector("#three").style.color="green";
+// console.log(document.querySelector("#three").style.color="green")
+// document.querySelector(".four").style.color="yellow";
+// console.log(document.querySelector(".four").style.color="yellow")
+// document.querySelector(".five").style.color="brown";
+// console.log(document.querySelector(".five").style.color="brown")
+// document.querySelectorAll("h1")
+console.log(document.querySelectorAll("h1")[0].style.color="steelblue")
